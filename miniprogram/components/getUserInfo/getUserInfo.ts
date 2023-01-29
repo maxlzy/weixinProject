9b150dfdc66b04f22dfec055323a95ef2a533f9d
maxlzy <80317614+maxlzy@users.noreply.github.com>
2023-01-29 17:29:29 +0800

    submit

--- miniprogram/components/getUserInfo/getUserInfo.ts
+++ miniprogram/components/getUserInfo/getUserInfo.ts
@@ -1,4 +1,5 @@
 // components/getUserInfo/getUserInfo.ts
+const appData = getApp<IAppOption>()
 Component({
   /**
    * 组件的属性列表
@@ -18,6 +19,18 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    getUserInfo() {
+      console.log("1111")
+      wx.login({
+        success: (res) => {
+          console.log(res)
+          wx.getUserInfo({
+            success: (res) => {
+              console.log(res)
+            }
+          })
+        }
+      })
+    }
   }
 })
